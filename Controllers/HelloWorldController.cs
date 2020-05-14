f3199a1c74f824e7540c0de6e57108b7f0aad8b9
RissaSams <rissa.sams@yahoo.com>
2020-05-14 12:15:35 -0500

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Marissa Dorman";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+
+        public IActionResult WhiteRabbit()
+        {
+            ViewBag.Name = "Marissa Dorman";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "People...the Techonology has you..";
+        }
+
+        public string GoodBye()
+        {
+            return "See ya, later!";
+        }
+
+        public string Exit()
+        {
+            return "This is the end!";
+        }
+    }
+}
